13599c5bf1a46f3038f36005b170cd42d589aec3
fuhaiyang <ocean@fuhaiyang.local>
2017-01-10 12:24:30 +0800

    update

--- MyFreeMall/Other/MFNavigationController.m
+++ MyFreeMall/Other/MFNavigationController.m
@@ -38,15 +38,19 @@
 - (void)pushViewController:(MFBaseViewController *)viewController animated:(BOOL)animated {
     if (self.viewControllers.count > 0) {
         // 替换back按钮
-        UIBarButtonItem *backBarButtonItem = [UIBarButtonItem barButtonItemWithImageName:@"icon_back"
-                                                                         imageEdgeInsets:UIEdgeInsetsMake(0, 0, 0, 8)
+        UIBarButtonItem *backBarButtonItem = [UIBarButtonItem barButtonItemWithImageName:@"返回"
+                                                                         imageEdgeInsets:UIEdgeInsetsMake(0, 0, 0, 20)
                                                                                   target:self
                                                                                   action:@selector(back)];
         
         viewController.navigationItem.leftBarButtonItem = backBarButtonItem;
         // 隐藏tabbar
         viewController.hidesBottomBarWhenPushed         = YES;
-        _canDragBack                                    = viewController.canDragBack;
+        if ([viewController respondsToSelector:@selector(canDragBack)]) {
+            _canDragBack = viewController.canDragBack;
+        }else{
+            _canDragBack = NO;
+        }
     }
     [super pushViewController:viewController animated:animated];
 }
